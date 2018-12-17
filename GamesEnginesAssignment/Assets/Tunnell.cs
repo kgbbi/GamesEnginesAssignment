57c79c70f4dcc571745d27d26fcfd51a40bd0594
Alex Hurduc <c15344251@mydit.ie>
2018-12-17 16:40:08 +0000

    added tunnel game object

---
+++ GamesEnginesAssignment/Assets/Tunnell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//creates multiple pipes together to generate the wormhole tunnel effect
+public class Tunnell : MonoBehaviour {
+
+    public Wormhole pipePrefab;
+
+    public int pipeCount;
+
+    private Wormhole[] pipes;
+
+    private void Awake()
+    {
+        pipes = new Wormhole[pipeCount];
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Wormhole pipe = pipes[i] = Instantiate<Wormhole>(pipePrefab);
+            pipe.transform.SetParent(transform, false);
+        }
+    }
+}
