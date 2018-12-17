d5fc73ba426a64c375880052758b2bae8d475cb4
Alex Hurduc <c15344251@mydit.ie>
2018-12-17 18:56:42 +0000

    Added player component for camera movement inside tunnel

---
+++ GamesEnginesAssignment/Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    public Tunnell pipeSystem;
+
+    public float velocity;
+
+    private Wormhole currentPipe;
+}
