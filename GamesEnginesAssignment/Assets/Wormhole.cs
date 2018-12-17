5fd376ff4f270b8454559935fd2f421f9fbc590f
Alex Hurduc <c15344251@mydit.ie>
2018-12-17 00:02:30 +0000

    Initialised wormhole as torus

---
+++ GamesEnginesAssignment/Assets/Wormhole.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wormhole : MonoBehaviour {
+
+    //A tunnel section is a partial circle. Or a partial torus in 3D
+    public float curveRadius, pipeRadius; //A torus is defined by two radiuses
+    public int curveSegmentCount, pipeSegmentCount;//i will use a mesh to create the pipe's surface
+}
