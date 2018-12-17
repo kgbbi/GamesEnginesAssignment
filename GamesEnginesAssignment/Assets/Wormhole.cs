d7d49404d9a2157fb17ba64f7984e9018cf9a9b2
Alex Hurduc <c15344251@mydit.ie>
2018-12-17 00:22:02 +0000

    Adding mesh for the tunnel

--- GamesEnginesAssignment/Assets/Wormhole.cs
+++ GamesEnginesAssignment/Assets/Wormhole.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wormhole : MonoBehaviour {
+public class Wormhole : MonoBehaviour
+{
 
     //A tunnel section is a partial circle. Or a partial torus in 3D
     public float curveRadius, pipeRadius; //A torus is defined by two radiuses
-    public int curveSegmentCount, pipeSegmentCount;//i will use a mesh to create the pipe's surface
+    public int curveSegmentCount, pipeSegmentCount; //i will use a mesh to create the pipe's surface
 
-    private Vector3 GetPointOnTorus(float u, float v)//find points on the surface using x = (R + r cos v) cos u
-                                                     //y = (R + r cos v) sin u
-                                                     //z = r cos v
+    private Vector3 GetPointOnTorus(float u, float v) //find points on the surface using x = (R + r cos v) cos u
+        //y = (R + r cos v) sin u
+        //z = r cos v
     {
         Vector3 p;
         float r = (curveRadius + pipeRadius * Mathf.Cos(v));
@@ -21,14 +22,40 @@ public class Wormhole : MonoBehaviour {
     }
 
 
-    private void OnDrawGizmos()//test whether torus displays
+    private void OnDrawGizmos() //test whether torus displays
     {
         float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
 
-        for (int v = 0; v < pipeSegmentCount; v++)
+        for (int u = 0; u < curveSegmentCount; u++)
         {
-            Vector3 point = GetPointOnTorus(0f, v * vStep);
-            Gizmos.DrawSphere(point, 0.1f);
+            //test for entire torus
+            for (int v = 0; v < pipeSegmentCount; v++)
+            {
+                Vector3 point = GetPointOnTorus(u * uStep, v * vStep);
+                Gizmos.color = new Color(
+                    1f,
+                    (float) v / pipeSegmentCount,
+                    (float) u / curveSegmentCount);
+                Gizmos.DrawSphere(point, 0.1f);
+            }
         }
     }
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Awake()
+    {
+        //create the mesh when the object awakens
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Wormhole";
+        SetVertices();
+        SetTriangles();
+    }
+
+    private void SetVertices() { }
+
+    private void SetTriangles() { }
 }
