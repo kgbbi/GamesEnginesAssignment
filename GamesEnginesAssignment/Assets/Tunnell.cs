b69e17685dee4ac6b655933717ad58c579ec6fd3
Alex Hurduc <c15344251@mydit.ie>
2018-12-17 22:22:19 +0000

    Moved camera to inside tunnel and fixed gaps when running through pipe

--- GamesEnginesAssignment/Assets/Tunnell.cs
+++ GamesEnginesAssignment/Assets/Tunnell.cs
@@ -24,13 +24,15 @@ public class Tunnell : MonoBehaviour {
                 pipe.AlignWith(pipes[i - 1]);
             }
         }
+        AlignNextPipeWithOrigin();
     }
+
 
     public Wormhole SetupFirstPipe()
     //provide the first pipe
     {
-        transform.localPosition = new Vector3(0f, -pipes[0].CurveRadius);
-        return pipes[0];
+        transform.localPosition = new Vector3(0f, -pipes[1].CurveRadius);
+        return pipes[1];
     }
 
     public Wormhole SetupNextPipe()
@@ -41,8 +43,8 @@ public class Tunnell : MonoBehaviour {
         AlignNextPipeWithOrigin();
         pipes[pipes.Length - 1].Generate();
         pipes[pipes.Length - 1].AlignWith(pipes[pipes.Length - 2]);
-        transform.localPosition = new Vector3(0f, -pipes[0].CurveRadius);
-        return pipes[0];
+        transform.localPosition = new Vector3(0f, -pipes[1].CurveRadius);
+        return pipes[1];
     }
 
     private void ShiftPipes()
@@ -56,18 +58,24 @@ public class Tunnell : MonoBehaviour {
     }
     private void AlignNextPipeWithOrigin()
     {//reset position and rotation of the new first pipe
-        Transform transformToAlign = pipes[0].transform;
-        for (int i = 1; i < pipes.Length; i++)
+        Transform transformToAlign = pipes[1].transform;
+        for (int i = 0; i < pipes.Length; i++)
         {
-            pipes[i].transform.SetParent(transformToAlign);
+            if (i != 1)
+            {
+                pipes[i].transform.SetParent(transformToAlign);
+            }
         }
 
         transformToAlign.localPosition = Vector3.zero;
         transformToAlign.localRotation = Quaternion.identity;
 
-        for (int i = 1; i < pipes.Length; i++)
+        for (int i = 0; i < pipes.Length; i++)
         {
-            pipes[i].transform.SetParent(transform);
+            if (i != 1)
+            {
+                pipes[i].transform.SetParent(transform);
+            }
         }
     }
 
